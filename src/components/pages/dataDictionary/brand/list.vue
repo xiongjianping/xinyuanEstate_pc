9d23cfb50702215465df60272020a5ab61d860f9
perryLiuPC <perrymrliu@foxmail.com>
2018-07-28 22:24:49 +0800

    业态管理 业种管理接口对接

--- src/components/pages/dataDictionary/brand/list.vue
+++ src/components/pages/dataDictionary/brand/list.vue
@@ -2,36 +2,40 @@
   <div class="mainContent" v-loading="loading" element-loading-text="拼命加载中">
     <el-row class="searchBox" :gutter="30">
       <el-form label-width="100px" :model="searchForm">
-
-
-
         <el-col :span="24" class="text-center">
           <el-form-item label-width="0">
-            <el-button type="primary" size="medium" v-on:click="searchList(1);">新增</el-button>
+            <el-button type="primary" size="medium" v-on:click="addDetail">新增</el-button>
           </el-form-item>
         </el-col>
       </el-form>
     </el-row>
     <div class="listCont">
-      <el-table :data="data.list" border size="medium">
-        <el-table-column align="center" type="id" prop="id" label="序号" width="50"></el-table-column>
-        <el-table-column align="center" prop="area" label="业态编号" width="200"></el-table-column>
-        <el-table-column align="center" prop="company" label="业态"  width="100"></el-table-column>
-        <el-table-column align="center" prop="startTime" label="修改时间"></el-table-column>
+      <el-table :data="data.resultList" border size="medium">
+        <el-table-column align="center" type="index"  label="序号" width="50"></el-table-column>
+        <el-table-column align="center" prop="num" label="业态编号" width="200"></el-table-column>
+        <el-table-column align="center" prop="name" label="业态"  width="100"></el-table-column>
+        <el-table-column align="center" prop="modifyTime" label="修改时间"></el-table-column>
         <el-table-column align="center"  label="操作" width="100">
           <template slot-scope="scope">
             <el-button type="text" v-on:click="editDetails(scope.row.id)">修改</el-button>
-            <el-button type="text" v-on:click="showDetails(scope.row)">删除</el-button>
+            <el-button type="text" v-on:click="deleteDetails(scope.row.id)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
-      <div class="block">
-        <span class="demonstration"></span>
-        <el-pagination
-          layout="prev, pager, next"
-          :total="30">
-        </el-pagination>
-      </div>
+      <el-row type='flex' justify="center">
+        <el-col :span="12">
+          <div class="block">
+            <span class="demonstration"></span>
+            <el-pagination
+              @current-change="handleCurrentChange"
+              :current-page="data.page"
+              :page-size="size"
+              layout="prev, pager, next"
+              :total="data.countSize">
+            </el-pagination>
+          </div>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -46,7 +50,6 @@ export default {
       company: '',
       startTime: null
     },
-    infoData: {},
     size: 10,
     dialogFormVisible: false,
     dialogVisible: false,
@@ -69,20 +72,14 @@ export default {
       this.loading = true
       var that = this
       var page
-      var params = {
-        publishedName: that.searchForm.publishedName ? that.searchForm.publishedName : null,
-        merchandise: that.searchForm.merchandise ? that.searchForm.merchandise : null,
-        startTime: that.searchForm.startTime ? moment(new Date(that.searchForm.startTime).getTime()).format('YYYY-MM-DD HH:mm:ss') : null
-      }
       if (type === 1) {
         page = 1
+        this.data.page = 1
       } else {
         page = this.data.page
       }
-      console.log(params, page)
-      that.loading = true
       // that.$axios.post('/shop/Appraise/queryAll?p=' + page + '&c=' + that.size, params).then((res) => {
-      that.$axios.get('/list').then((res) => {
+      that.$axios.get('/base/find/business/form?p=' + page + '&c=' + this.size).then(res => {
         that.loading = false
         that.data = res
       }).catch(function (eMsg) {
@@ -91,11 +88,53 @@ export default {
       })
     },
     // 查看详情
-    showDetails (id) {
-      this.$router.push('/dataDicionary/brand/details/' + id)
+    deleteDetails (id) {
+      // this.$router.push('/dataDicionary/brand/details/' + id)
+      this.$axios.get('/base/remove/business/form/' + id).then(res => {
+        if (res === 'OK') {
+          this.$alert('删除成功!','提示',{
+            type:'success'
+          }).then(res => {
+            this.searchList(1)
+          })
+        }
+      }).catch(err => {
+        console.log(err)
+        this.showAlert(err)
+      })
     },
     editDetails (id) {
-      this.$router.push('/projecManage/edit/' + id)
+      // this.$router.push('/projecManage/edit/' + id)
+
+    },
+    addDetail(){
+      this.$prompt('请输入业态名称', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputPattern: /^.{0,20}$/,
+          inputErrorMessage: '名称长度不得超过20个字'
+        }).then(val => {
+          if (val.value === null  || val.value.replace(/\s/g,'') === '') {
+            this.$alert('请不要输入空字符','提示',{
+              type:'error'
+            })
+          } else {
+            this.$axios.get('/base/save/business/form/' + val.value.replace(/\s/g,'')).then(res => {
+              console.log(res)
+              if (res === 'OK') {
+                this.$alert('添加成功','提示',{
+                  type:'success'
+                }).then(res => {
+                  this.searchList(1)
+                })
+              }
+            }).catch (err => {
+              this.$alert(err,'提示',{
+                  type:'error'
+                })
+            })
+          }
+        })
     },
     showAlert: function (cont) {
       this.$alert(cont, '温馨提示', {
