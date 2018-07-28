2a9b82a41f44e1bd40e4b152fe36c2461a827cb9
unknown <18234769507@163.com>
2018-07-28 15:53:30 +0800

    评估模板

--- src/router/index.js
+++ src/router/index.js
@@ -574,17 +574,17 @@ export default new Router({
             pageName: '评估模板'
           },
           component: resolve => require(['@/components/common/SubContent.vue'], resolve),
-          children: [
-            {
-              path: 'organization',
-              name: 'organization',
-              meta: {
-                pageName: '模板管理'
-              },
-              component: resolve => require(['@/components/common/SubContent.vue'], resolve),
+          // children: [
+          //   {
+          //     path: 'organizationStructure',
+          //     name: 'organizationStructure',
+          //     meta: {
+          //       pageName: '模板管理'
+          //     },
+          //     component: resolve => require(['@/components/common/SubContent.vue'], resolve),
               children:[
                 {
-                  path: 'list',
+                  path: 'organization',
                   name: 'organizationList',
                   meta: {
                     parentPath: '/organizationStructure',
@@ -610,8 +610,8 @@ export default new Router({
                   },
                   component: resolve => require(['@/components/pages/organizationStructure/organization/details.vue'], resolve)
                 }
-              ]
-            },
+            //   ]
+            // },
           ]
         },
 
